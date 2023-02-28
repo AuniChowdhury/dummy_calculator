1f3706d82dffa14ddcb08ed336693f8f664b75fb
samia.chowdhury <samia.chowdhury@infolytx.com>
2023-02-28 16:41:45 +0600

    add func

--- calculator_service/calculator_service_impl.py
+++ calculator_service/calculator_service_impl.py
@@ -3,3 +3,17 @@ class Calculator:
     @staticmethod
     def add_numbers(first_num: int, second_num: int, third_num: int):
         return first_num + second_num
+
+    # This function returns a string for an integer
+    @staticmethod
+    def func_x(num):
+        if num == 1:
+            return "1"
+        elif num == 2:
+            return "2"
+        elif num == 3:
+            return "3"
+        elif num == 4:
+            return "4"
+        elif num == 5:
+            return "5"
