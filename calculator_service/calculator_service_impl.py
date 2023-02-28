d1a83c3c7f55526cc9627d06a295a6b3d4d56cbd
samia.chowdhury <samia.chowdhury@infolytx.com>
2023-02-28 16:17:22 +0600

    add function

--- calculator_service/calculator_service_impl.py
+++ calculator_service/calculator_service_impl.py
@@ -4,3 +4,5 @@ class Calculator:
     def add_numbers(first_num: int, second_num: int, third_num: int):
         return first_num + second_num
 
+    def multiply(self, a: int, b: int):
+        return a * b
