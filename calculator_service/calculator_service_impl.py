038fa7ce62c51be7c675b65f87b1e01a0c4bf482
samia.chowdhury <samia.chowdhury@infolytx.com>
2023-02-28 15:33:43 +0600

    add subtraction method

--- calculator_service/calculator_service_impl.py
+++ calculator_service/calculator_service_impl.py
@@ -3,3 +3,7 @@ class Calculator:
     @staticmethod
     def add_numbers(first_num: int, second_num: int):
         return first_num + second_num
+
+    @staticmethod
+    def xyz(a: int, b: int):
+        return a-b
