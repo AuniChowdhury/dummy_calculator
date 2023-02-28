ce1931550f5c7a1f5a1c3cffc20e1fae76f32845
samia.chowdhury <samia.chowdhury@infolytx.com>
2023-02-28 15:36:46 +0600

    remove static decorator

--- calculator_service/calculator_service_impl.py
+++ calculator_service/calculator_service_impl.py
@@ -4,6 +4,5 @@ class Calculator:
     def add_numbers(first_num: int, second_num: int):
         return first_num + second_num
 
-    @staticmethod
-    def xyz(a: int, b: int):
+    def xyz(self, a: int, b: int):
         return a-b
