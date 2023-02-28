52eff2a4711f70f6bab16b3982ff890cf5b4bbd1
samia.chowdhury <samia.chowdhury@infolytx.com>
2023-02-28 16:19:38 +0600

    remove func

--- calculator_service/calculator_service_impl.py
+++ calculator_service/calculator_service_impl.py
@@ -3,6 +3,3 @@ class Calculator:
     @staticmethod
     def add_numbers(first_num: int, second_num: int, third_num: int):
         return first_num + second_num
-
-    def multiply(self, a: int, b: int):
-        return a * b
